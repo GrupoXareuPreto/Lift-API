9213e424765a973c3a7d8213b066a4c01b8f3d51
Etec Prof. Horácio Augusto da Silveira <jjmonstro>
2025-08-11 11:46:28 -0300

    primeiro commit

---
+++ lift/src/test/java/br/com/xareu/lift/LiftApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xareu.lift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
