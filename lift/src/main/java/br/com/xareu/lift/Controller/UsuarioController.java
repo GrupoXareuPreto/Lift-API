90dcb1e7f4b3c08ad539651d8cea13746ac27e71
NogPiss <gorduranogueirapissuto@gmail.com>
2025-08-21 21:52:09 -0300

    crud usuario, falta o update

--- lift/src/main/java/br/com/xareu/lift/Controller/UsuarioController.java
+++ lift/src/main/java/br/com/xareu/lift/Controller/UsuarioController.java
@@ -3,9 +3,11 @@ package br.com.xareu.lift.Controller;
 import br.com.xareu.lift.Entity.Usuario;
 import br.com.xareu.lift.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -15,8 +17,13 @@ public class UsuarioController {
     private UsuarioService service;
 
     @GetMapping
-    public List<Usuario> listar(){
+    public List<Usuario> listarTodos(){
         return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id){
+        return service.buscarPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
@@ -24,9 +31,28 @@ public class UsuarioController {
         return service.criarUsuario(usuario);
     }
 
-    @GetMapping("{id}")
-    public Usuario get(@PathVariable Long id){
-            return service.get(id);
+    @PutMapping("/{id}")
+    public Usuario atualizarUsuario(@RequestBody Usuario usuario,  @PathVariable Long id){
+
+
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarUsuario(@PathVariable Long id){
+        boolean deletado = service.deletarUsuario(id);
+
+        if(deletado){
+            return ResponseEntity.ok().body("Usuario deletado com sucesso");
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /*
+    Editar
+    Excluir
+     */
+
 
 }
--- lift/src/main/java/br/com/xareu/lift/Service/UsuarioService.java
+++ lift/src/main/java/br/com/xareu/lift/Service/UsuarioService.java
@@ -2,9 +2,11 @@ package br.com.xareu.lift.Service;
 
 import br.com.xareu.lift.Entity.Usuario;
 import br.com.xareu.lift.Repository.UsuarioRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -22,7 +24,39 @@ public class UsuarioService {
         return usuarioRepository.save(usuario);
     }
 
-    public Usuario getById(Long id){ usuarioRepository.findById()}
+    public Optional<Usuario> buscarPorId(Long id){
+        try{
+            return usuarioRepository.findById(id);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return Optional.empty();
+    }
+
+    public boolean atualizarUsuario(Usuario usuario, Long id){
+        boolean existe = usuarioRepository.findById(id).isPresent();
+
+        if(existe){
+
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean deletarUsuario(Long id){
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if(usuario.isPresent()){
+            usuarioRepository.deleteById(id);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
 
 
 
