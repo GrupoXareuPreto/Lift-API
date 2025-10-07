d2b96828321276ca7159c48a8fc925f529a7b335
NogPiss <gorduranogueirapissuto@gmail.com>
2025-10-06 22:38:28 -0300

    atualizacao das DTOs de eventos

---
+++ lift/src/main/java/br/com/xareu/lift/DTO/Evento/EventoResponseDTO.java
@@ -0,0 +1,13 @@
+package br.com.xareu.lift.DTO.Evento;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EvenyoResponseDTO {
+
+
+}
